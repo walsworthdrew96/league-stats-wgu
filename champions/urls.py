ade59ae5153855e159832ac3c8cff759fdb1cef7
Drew Walsworth <dwalswo@wgu.edu>
2020-07-27 15:20:01 -0500

    final commit

--- champions/urls.py
+++ champions/urls.py
@@ -5,12 +5,9 @@ urlpatterns = [
     path('', views.champions),
     path('champions/', views.champions),
     path('champion/<str:champion_name>/', views.champion, name='urlname'),
-    # path('import_data/', views.import_data),
     path('about/', views.about),
     path('1v1_simulator/', views.fight_simulator),
-    # path('champion_form_view/', views.champion_form_view),
-    # path('post/ajax/champion/', views.post_champion, name="post_champion"),
-    # path('get/ajax/validate/name/', views.check_name, name="validate_name"),
     path('get/ajax/champion_graph/', views.get_graph_data, name="get_graph_data"),
-    path('get/ajax/1v1_simulation/', views.get_simulation_results, name="get_simulation_results")
+    path('get/ajax/1v1_simulation/', views.get_simulation_results, name="get_simulation_results"),
+    # path('import_data/', views.import_data)
 ]
